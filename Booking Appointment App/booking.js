b01445a9b39bae52e9e0866a9c8a985303966e0a
abhi9871 <at719273@gmail.com>
2023-06-21 01:42:53 +0530

    Storing data as an object in the local storage

--- Booking Appointment App/booking.js
+++ Booking Appointment App/booking.js
@@ -8,41 +8,21 @@ function submitForm(event){
     var phone = document.getElementById('phone').value;
     var date = document.getElementById('call').value;
     var time = document.getElementById('time').value;
-    
-    // One way to store data into local storage
-    // localStorage.setItem('name', name);
-    // localStorage.setItem('mail', email);
-    // localStorage.setItem('phone', phone);
-    // localStorage.setItem('call', date);
-    // localStorage.setItem('time', time);
 
-    // Second way to store data into local storage
     // Create a data object
-        // var formData = {
-        // Name: name,
-        // Mail: email,
-        // Phone: phone,
-        // Date: date,
-        // Time: time
-        // };
-
-        // // Convert the data object to a JSON string
-        // var formDataString = JSON.stringify(formData);
-
-        // // Store the JSON string in localStorage
-        // localStorage.setItem('formData', formDataString);
-
-        // Third way to store data into local storage
         var formData = {
-                name: name,
-                mail: email,
-                phone: phone,
-                date: date,
-                time: time
-            };
-                // Store the data in localStorage
-                for (var key in formData) {
-                    localStorage.setItem(key, formData[key]);
-            }
+        Name: name,
+        Mail: email,
+        Phone: phone,
+        Date: date,
+        Time: time
+        };
+
+        // Convert the data object to a JSON string
+        var formDataString = JSON.stringify(formData);
+
+        // Store the JSON string in localStorage
+        localStorage.setItem('formData', formDataString);
+
             form.reset();
 }
