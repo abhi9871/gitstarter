5461be339297453013497552304b971c3e9e1005
abhi9871 <at719273@gmail.com>
2023-06-21 16:15:07 +0530

    Delete specific user local storage data by del btn

--- Booking Appointment App/booking.js
+++ Booking Appointment App/booking.js
@@ -28,15 +28,30 @@ function submitForm(event){
         // To show the user details at the bottom of the form
         var li = document.createElement('li');
         li.appendChild(document.createTextNode(`${name} - ${email} - ${phone} - ${date} - ${time}`));
+
+        // Add a delete button to a list
         var delBtn = document.createElement('button');
         delBtn.className = 'btn btn-info mx-2 my-2';
         delBtn.appendChild(document.createTextNode('Delete'));
         li.appendChild(delBtn);
+
+        // Add an edit button to a list
+        var editBtn = document.createElement('button');
+        editBtn.className = 'btn btn-info mx-2 my-2';
+        editBtn.appendChild(document.createTextNode('Edit'));
+        li.appendChild(editBtn);
+
+        // Append list to a ul
         userList.appendChild(li);
+
+        // Add event listener to the delete button
         delBtn.addEventListener('click', function(e) {
             var li = e.target.parentElement;
+            var email = li.textContent.split('-')[1].trim();
             userList.removeChild(li);
-            localStorage.clear();
+            localStorage.removeItem(email);
             form.reset();
-        })    
+        })
+        
+        
 }
