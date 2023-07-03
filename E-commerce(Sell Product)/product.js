fb002cf26464b45352dfab62842a7eefa10d99d7
abhi9871 <at719273@gmail.com>
2023-07-03 15:21:45 +0530

    Update request with async-await and comment added

--- E-commerce(Sell Product)/product.js
+++ E-commerce(Sell Product)/product.js
@@ -1,95 +1,128 @@
-let productForm = document.getElementById('sellingProduct');
-let productList = document.getElementById('productList');
+// Get the form element with the ID "sellingProduct"
+let productForm = document.getElementById("sellingProduct");
+
+// Get the ul element with the ID "productList"
+let productList = document.getElementById("productList");
 
 // Add an event listener to a form
-productForm.addEventListener('submit', sellProductForm);
+productForm.addEventListener("submit", sellProductForm);
 
-function sellProductForm(e){
-    e.preventDefault(); // Prevent form submission to avoid page refresh
-    let sellingPrice = document.getElementById('sellingPrice');
-    let productName =  document.getElementById('productName');
+async function sellProductForm(e) {
+  e.preventDefault(); // Prevent form submission to avoid page refresh
 
-    let obj = {
-        price : sellingPrice.value,
-        productName : productName.value
-    }
-        
-    // Create data into backend
-    axios.post('https://crudcrud.com/api/4f5784f523f141bcbcb7493f9396a4e5/products', obj)
-    .then((response) => {
-        console.log('Data has been created');
-        let data = response.data;
-        showProducts(data);
-    })
-    .catch((err) => console.log(err));
+  let sellingPrice = document.getElementById("sellingPrice");
+  let productName = document.getElementById("productName");
+
+  // Creating an object for passing into the post request
+  let productsObj = {
+    price: sellingPrice.value,
+    productName: productName.value,
+  };
+
+  // Create product into backend
+  try{
+    let createData = await axios.post("https://crudcrud.com/api/f4c4e85ae1904a85955b43f17599e7fa/products", productsObj);
+    console.log(`Data has been created: ${JSON.stringify(createData.data)}`);
+    let data = createData.data;
+    showProducts(data);
     productForm.reset();
+}
+  catch(err){
+    console.log(err);
+  }
 }
 
 // Show products and their prices on the screen
-function showProducts(res){
-    // create list
-    let li = document.createElement('li');
-    li.className = 'py-2';
-    li.setAttribute('id', `${res._id}`)
-    li.appendChild(document.createTextNode(`${res.price} - ${res.productName}`));
+function showProducts(res) {
+  // create list
+  let li = document.createElement("li");
 
-    // create delete button
-    let deleteBtn = document.createElement('button');
-    deleteBtn.className = 'btn btn-secondary btn-sm float-right mx-2';
+  // Adding class to the list
+  li.className = "py-2";
 
-    deleteBtn.appendChild(document.createTextNode('Delete Product'));
+  //Setting an attribute to the list
+  li.setAttribute("id", `${res._id}`);
 
-    // append delete button to li
-    li.appendChild(deleteBtn);
+  // Appending and creating the textNode for the list
+  li.appendChild(document.createTextNode(`${res.price} - ${res.productName}`));
 
-    // finally append li to ul(product list)
-    productList.appendChild(li);
+  // create delete button
+  let deleteBtn = document.createElement("button");
 
-    let productPrice = document.getElementById('productPrice');
-    calculatePrice(`${res.price}`);
-    productPrice.textContent = `Total value worth of Products: Rs. ${total}`;
+  // Adding classes to the deleteBtn
+  deleteBtn.className = "btn btn-secondary btn-sm float-right mx-2";
 
-    // add delete button functionality
-    deleteBtn.addEventListener('click', (e) => {
-        let li = e.target.parentElement;
-        let id = li.getAttribute('id');
-        let price = parseInt(li.firstChild.textContent.split(' - ')[0]);
-        productList.removeChild(li);
-        updateTotalPrice(price);
-        deleteProducts(id);
-    })
+  // Appending and creating the textNode for the delete button
+  deleteBtn.appendChild(document.createTextNode("Delete Product"));
+
+  // append delete button to li
+  li.appendChild(deleteBtn);
+
+  // finally append li to ul(product list)
+  productList.appendChild(li);
+
+  // Get the heading element with the ID "productPrice"
+  let productPrice = document.getElementById("productPrice");
+
+  // Calculating the price and updating the productPrice.textContent
+  calculatePrice(`${res.price}`);
+  productPrice.textContent = `Total value worth of products: Rs. ${total}`;
+
+  // add delete button functionality
+  deleteBtn.addEventListener("click", (e) => {
+    let li = e.target.parentElement;
+
+    // Getting the id using attribute
+    let id = li.getAttribute("id");
+
+    // Getting the price after spliting the firstChild textContent
+    let price = parseInt(li.firstChild.textContent.split(" - ")[0]);
+
+    // Removing the list from the productList
+    productList.removeChild(li);
+
+    // After removing we are updating the price
+    updateTotalPrice(price);
+
+    // Invoking the function to delete the product for a particular id
+    deleteProducts(id);
+  });
 }
 
 // Show products on onload
-window.addEventListener('DOMContentLoaded', () => {
-    axios.get('https://crudcrud.com/api/4f5784f523f141bcbcb7493f9396a4e5/products')
-    .then(response => {
-        console.log(response.data);
-        response.data.forEach(product => {
-            showProducts(product);
-        });
-    })
-    .catch(err => console.log(err));
-})
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    let showData = await axios.get("https://crudcrud.com/api/f4c4e85ae1904a85955b43f17599e7fa/products");
+    console.log(showData.data);
+    showData.data.forEach((product) => {
+      showProducts(product);
+    });
+  } 
+  catch (err) {
+    console.log(err);
+  }
+});
 
 // delete products by id
-function deleteProducts(id) {
-    axios.delete(`https://crudcrud.com/api/4f5784f523f141bcbcb7493f9396a4e5/products/${id}`)
-    .then((response) => {
-        console.log('Products has been deleted');
-    })
-    .catch((err) => console.log(err))
+async function deleteProducts(id) {
+  try {
+    await axios.delete(`https://crudcrud.com/api/f4c4e85ae1904a85955b43f17599e7fa/products/${id}`);
+    console.log('Product has been deleted');
+  } 
+  catch (err) {
+    console.log(err);
+  }
 }
 
 let total = 0;
 // calculate product price
-function calculatePrice(price){ 
-    total += parseInt(price);
+function calculatePrice(price) {
+  total += parseInt(price);
 }
 
 // Update total price on the screen
 function updateTotalPrice(price = 0) {
-    total -= parseInt(price);
-    let productPrice = document.getElementById('productPrice');
-    productPrice.textContent = `Total value worth of Products: Rs. ${total}`;
-  }
+  total -= parseInt(price);
+  let productPrice = document.getElementById("productPrice");
+  productPrice.textContent = `Total value worth of products: Rs. ${total}`;
+}
