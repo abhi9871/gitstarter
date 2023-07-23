20f8c6f6d9763b29d28161e173964958426699ab
abhi9871 <at719273@gmail.com>
2023-07-23 05:43:34 +0530

    Applied crud operation using backend api

--- Expense Tracker App/main.js
+++ Expense Tracker App/main.js
@@ -1,41 +1,47 @@
 let form = document.getElementById('expenseForm');
 let displayExpense = document.getElementById('expenseDetails');
-
+let isEditing = false; // Flag to indicate if we are editing an expense details or not
 form.addEventListener('submit', addExpense);
 
 // Add expense
-function addExpense(e) {
+ function addExpense(e) {
     e.preventDefault(); // Prevent form submission to avoid page refresh
-    let expenseAmout = document.getElementById('expenseAmount');
+    let expenseAmount = document.getElementById('expenseAmount');
     let expenseDescription = document.getElementById('expenseDescription');
     let expenseCategory = document.getElementById('expenseCategory');
 
     // Create expense data object
     let expenseData = {
-        amount : expenseAmout.value,
-        descripton : expenseDescription.value,
-        category : expenseCategory.value 
+        expenseAmount : expenseAmount.value,
+        expenseDescription : expenseDescription.value,
+        expenseCategory : expenseCategory.value 
     }
 
-    // Convert the expense data object to a JSON string
-    let expenseStringData = JSON.stringify(expenseData);
+    if(isEditing) {
+        // Updating an expense details
+        let expenseId = form.getAttribute('expenseId');
+        let li = document.getElementById(expenseId);
+        li.firstChild.textContent = `\u20B9${expenseData.expenseAmount} - ${expenseData.expenseCategory} - ${expenseData.expenseDescription}`
+        editExpense(expenseId, expenseData);
+    } else {
+        // Create an expense
+        createExpense(expenseData);
+    }
+    form.reset();
+}
 
-    //Create a key for localStorage
-    let expenseKey = generateExpenseKey();
-
-    // Store into local storage
-    localStorage.setItem(expenseKey, expenseStringData);
-
+// Show expense details on user screen
+function showExpenseDetails(expenseData) {
     // Create a list element
     let li = document.createElement('li');
 
-    // Add attribute to the list
-    li.setAttribute('data-key', expenseKey);
-
     li.className = 'py-2';
 
+    // set an id attribute for li element
+    li.setAttribute('id', expenseData.id);
+
     // Create textNode for li and append to it
-    li.appendChild(document.createTextNode(`${expenseData.amount} - ${expenseData.category} - ${expenseData.descripton}`));
+    li.appendChild(document.createTextNode(`\u20B9${expenseData.amount} - ${expenseData.category} - ${expenseData.description}`));
 
     // Add edit expense button
     let editBtn = document.createElement('button');
@@ -59,33 +65,83 @@ function addExpense(e) {
     displayExpense.appendChild(li);
 
     // Edit an expense
-    editBtn.addEventListener('click', function(e){
+    editBtn.addEventListener('click', (e) => {
         let li = e.target.parentElement;
-        let key = li.getAttribute('data-key');
-        var stringDetails = li.firstChild.textContent.split('-'); 
-        expenseAmout.value = stringDetails[0].trim();
-        expenseCategory.value = stringDetails[1].trim();
-        expenseDescription.value = stringDetails[2].trim();
-
-        displayExpense.removeChild(li);
-        localStorage.removeItem(key);
+        let expenseId = li.getAttribute('id');
+        form.setAttribute('expenseId', expenseId);
+        getExpense(expenseId);
     })
 
     // Delete an expense
-    deleteBtn.addEventListener('click', function(e){
+    deleteBtn.addEventListener('click', (e) => {
         let li = e.target.parentElement;
-        let key = li.getAttribute('data-key');
+        let expenseId = li.getAttribute('id');
         displayExpense.removeChild(li);
-        localStorage.removeItem(key);
-    })
-
-    form.reset();
-
+        deleteExpense(expenseId);
+    });
 }
 
-// Create expense key function for generating dynamic keys for each entry
-
-function generateExpenseKey(){
-    let timestamp = new Date().getTime();
-    return `expense_${timestamp}`;
+// get all the expenses
+async function getExpenses() {
+    try {
+        const expenses = await axios.get('http://localhost:3000/expense/get-expenses');
+        const expensesData = expenses.data;
+        expensesData.forEach((expense) => {
+            showExpenseDetails(expense);
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
+
+// get an expense by id
+async function getExpense(expenseId) {
+    try {
+        isEditing = true;
+        let expenseAmount = document.getElementById('expenseAmount');
+        let expenseDescription = document.getElementById('expenseDescription');
+        let expenseCategory = document.getElementById('expenseCategory');
+
+      const expense = await axios.get(`http://localhost:3000/expense/get-expense/${expenseId}`);
+      expenseAmount.value = expense.data.amount;
+      expenseCategory.value = expense.data.category;
+      expenseDescription.value = expense.data.description;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// create an expense
+async function createExpense(expenseData) {
+    try {
+        const expense = await axios.post('http://localhost:3000/expense/create-expense', expenseData);
+        console.log(expense.data);
+        showExpenseDetails(expense.data);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// edit an expense
+async function editExpense(expenseId, expenseData) {
+    try {
+        isEditing = false;
+        const updatedExpense = await axios.put(`http://localhost:3000/expense/edit-expense/${expenseId}`, expenseData);
+        console.log(updatedExpense.data);
+    } catch (err) {
+       console.log(err); 
+    }
+}
+
+// delete an expense
+async function deleteExpense(expenseId) {
+    try {
+        const deletedExpense = await axios.delete(`http://localhost:3000/expense/delete-expense/${expenseId}`);
+        console.log('Expense deleted');
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Show expense data on onload
+window.addEventListener("DOMContentLoaded", getExpenses);
